ad0f337cfeb98cdb9333615afe17944a9b07e319
Tobias Gesellchen <tobias@gesellix.de>
2020-03-05 21:00:50 +0100

    Add task with non-cachable output

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
   groovy
   `java-library`
@@ -10,4 +13,22 @@ repositories {
 dependencies {
   implementation("org.codehaus.groovy:groovy-all:2.5.9")
   testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
+}
+
+tasks {
+  val buildInfoTask = create("buildInfo") {
+    val buildInfoFile = file("${buildDir}/resources/main/META-INF/build-info.properties")
+    outputs.file(buildInfoFile)
+    outputs.upToDateWhen { false }
+    doFirst {
+      val timestamp = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now())
+      mkdir(buildInfoFile.parent)
+      buildInfoFile.createNewFile()
+      buildInfoFile.writeText("version=${timestamp}")
+    }
+    doLast {
+      logger.lifecycle("build-info: ${buildInfoFile.readText()}")
+    }
+  }
+  listOf(processResources, compileJava, compileGroovy).forEach { it.get().dependsOn(buildInfoTask) }
 }
